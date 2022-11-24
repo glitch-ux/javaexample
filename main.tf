82222e2a4b57254a02522f46747d195de454a25d
glitch-ux <71816777+glitch-ux@users.noreply.github.com>
2022-11-24 00:53:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
   
-  
   site_config { 
     application_stack {
     java_server = "TOMCAT"
